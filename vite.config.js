b1809d4ea01f232f1c114e633608744e8296872c
Shiftyrin <shiftyrin@example.com>
2026-04-01 17:53:44 +0700

    Configure Vite for GitHub Pages deployment via Actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Dicky-Sofjan-Blog/',
   plugins: [react()],
   server: {
     proxy: {
